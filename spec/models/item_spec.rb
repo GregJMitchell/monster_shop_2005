2118919627bdf49650dd5f0570d7eaef971362c4
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-07 12:34:00 -0700

    Remove pry from test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -175,7 +175,6 @@ describe Item, type: :model do
 
     describe "class methods" do
       before(:each) do
-        require 'pry'; binding.pry
         @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
         @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
         @string = @bike_shop.items.create(name: "String", description: "It'll probably break!", price: 5, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", active?: false, inventory: 5)
