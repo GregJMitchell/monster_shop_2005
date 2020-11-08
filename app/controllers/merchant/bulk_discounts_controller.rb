96f82ccb1c163998ef1b4e8762741a074201b61a
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-07 17:02:52 -0700

    Add update and destroy actions to controller

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -28,6 +28,34 @@ class Merchant:: BulkDiscountsController < Merchant::BaseController
     end
   end
 
+  def edit
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    item = Item.find_by_merchant(params[:bulk_discount][:item_name], params[:bulk_discount][:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+    if item
+      @bulk_discount.update(discount_params.merge(item_id: item.id))
+      if @bulk_discount.save
+        flash[:notice] = "Your discount has been updated"
+        redirect_to "/merchant/bulk_discounts/#{@bulk_discount.id}"
+      else
+        flash[:error] = @bulk_discount.errors.full_messages.to_sentence
+        render :edit
+      end
+    else
+      flash[:error] = "An item by that name at your merchant does not exist"
+      render :edit
+    end
+  end
+
+  def destroy
+    @bulk_discount = BulkDiscount.find(params[:id])
+    @bulk_discount.destroy
+    redirect_to merchant_bulk_discounts_path
+  end
+
   private
 
   def discount_params
