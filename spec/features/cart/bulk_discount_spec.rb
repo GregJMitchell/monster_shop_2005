9cbe83137a055f2b56defe0d7de8adf4a260c654
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-08 20:07:20 -0700

    Add test to make sure the order show page has new discounted price

--- spec/features/cart/bulk_discount_spec.rb
+++ spec/features/cart/bulk_discount_spec.rb
@@ -12,38 +12,85 @@ describe 'As a regular user' do
         discount = create(:bulk_discount, merchant_id: merchant_user.merchant_id, item_id: item.id, quantity: 2, discount: 10)
         user = create(:user, email: 'user@gmail.com', password: 'password')
 
-        visit "/login"
-    
+        visit '/login'
+
         fill_in :email, with: user.email
-        fill_in :password, with: "password"
-        click_button "Login"
+        fill_in :password, with: 'password'
+        click_button 'Login'
 
         visit "/items/#{item.id}"
-        
-        click_on "Add To Cart"
-        
+
+        click_on 'Add To Cart'
+
         visit "/items/#{item_2.id}"
 
-        click_on "Add To Cart"
+        click_on 'Add To Cart'
 
-        visit "/cart"
+        visit '/cart'
 
         within "#cart-item-#{item.id}" do
-          click_on "+"
+          click_on '+'
         end
 
         discount_total = 2 * (item.price * (discount.discount / 100))
         item_subtotal = 2 * item.price - discount_total
 
         within "#cart-item-#{item.id}" do
-          within "#cart-subtotal" do
+          within '#cart-subtotal' do
             expect(page).to have_content(number_to_currency(item_subtotal))
           end
         end
 
-        within "#cart-grandtotal" do
+        within '#cart-grandtotal' do
           expect(page).to have_content(number_to_currency(item_subtotal + item_2.price))
         end
+      end
+
+      it 'When I place an order, the price on the order show page is the new discounted price' do
+        merchant_user = create(:merchant_employee, email: 'merchant@gmail.com', password: 'password')
+        item = create(:item, merchant: merchant_user.merchant)
+        item_2 = create(:item)
+        discount = create(:bulk_discount, merchant_id: merchant_user.merchant_id, item_id: item.id, quantity: 2, discount: 10)
+        user = create(:user, email: 'user@gmail.com', password: 'password')
+
+        visit '/login'
+
+        fill_in :email, with: user.email
+        fill_in :password, with: 'password'
+        click_button 'Login'
+
+        visit "/items/#{item.id}"
+
+        click_on 'Add To Cart'
+
+        visit "/items/#{item_2.id}"
+
+        click_on 'Add To Cart'
+
+        visit '/cart'
+
+        within "#cart-item-#{item.id}" do
+          click_on '+'
+        end
+        click_on 'Checkout'
+
+        fill_in :name, with: user.name
+        fill_in :address, with: user.address
+        fill_in :city, with: user.city
+        fill_in :state, with: user.state
+        fill_in :zip, with: user.zip
+
+        click_button 'Create Order'
+
+        new_order = Order.last
+
+        visit "/profile/orders/#{new_order.id}"
+
+        discount_total = 2 * (item.price * (discount.discount / 100))
+        item_subtotal = 2 * item.price - discount_total
+
+        expect(page).to have_content(number_to_currency(item_subtotal))
+        expect(page).to have_content(number_to_currency(item_subtotal + item_2.price))
       end
     end
   end
