6b66f80676dfdb5d009a56a300ab1865d96aa30d
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-09 17:29:01 -0700

    change price to be calculated by new_price method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,7 @@ class OrdersController < ApplicationController
       cart.items.each do |item, quantity|
         order.item_orders.create({ item: item,
                                    quantity: quantity,
-                                   price: cart.subtotal(item),
+                                   price: cart.new_price(item),
                                    merchant_id: item.merchant.id,
                                    status: 'Pending' })
       end
